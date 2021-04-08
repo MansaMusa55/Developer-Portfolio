c7482f0f98f53158e04a1eb1ce3ef471615e90c3
Samuel Musa <musax025@gmail.com>
2021-04-08 16:15:31 -0500

    Deploy Netlify

---
+++ js/site.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_6rpic2w';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      Swal.fire({
+        title: 'Email Sent',
+        text: 'Your email was sent succesfully, I will respond within 24 hours',
+        icon: 'success',
+        confirmButtonText: 'Cool'
+      })      
+    }, (err) => {
+      btn.value = 'Send Email';
+      Swal.fire({
+        title: 'Error!',
+        html: 'Looks like something went wrong! <hr /> I\'m always available on <a href="https://www.Linkedin.com/in/samuel-musa-27a21120b" target="_blank">LinkedIn</a>',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      })      
+      alert(JSON.stringify(err));
+    });
+});
